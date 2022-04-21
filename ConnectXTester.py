c5dac510e94318c1c87f962a14b5580a0657a894
Kemme Nash, Dever Patrick <dpkemmen@iu.edu>
2022-04-21 15:44:08 -0400

    changed results messages

--- ConnectXTester.py
+++ ConnectXTester.py
@@ -20,16 +20,16 @@ class ConnectXTester():
                 if (winner == 0):
                     print(f'Game {game+1} was a tie')
                 if (winner == 1):
-                    print(f'Player One ({self.player_one_name}) won Game {game+1}')
+                    print(f'Player 1 ({self.player_one_name}) won Game {game+1}')
                 if (winner == -1):
-                    print(f'Player Two ({self.player_two_name}) won Game {game+1}')
+                    print(f'Player 2 ({self.player_two_name}) won Game {game+1}')
         player_one_wins = len([x for x in results if x == 1])
         player_two_wins = len([x for x in results if x == -1])
         ties = len([x for x in results if x == 0])
         player_one_win_percent = player_one_wins/len(results)
         player_two_win_percent = player_one_wins/len(results)
         ties_percent = ties/len(result)
-        print(f'Reults from {self.num_games+1} Games')
+        print(f'Reults from {self.num_games+1} Games between {self.player_one_name} (Player 1) and {self.player_two_name} (Player 2)')
         print(f'Player 1 ({self.player_one_name} Win Percentage: {player_one_win_percent}')
         print(f'Player 2 ({self.player_two_name} Win Percentage: {player_two_win_percent}')
         print(f'Tie Percentage: {ties_percent}')
